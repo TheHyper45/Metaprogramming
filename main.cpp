af8568fcc54c898627caaf8ffb6ae212a6c8b3b5
TheHyper45 <dawid.priv@o2.pl>
2021-09-28 16:12:52 +0200

    Fun C++ usage

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+#include <tuple>
 #include <iostream>
+
+template<typename>
+struct function_traits;
+
+template<typename type,typename... types>
+struct function_traits<type(types...)>
+{
+    using return_type = type;
+
+    static inline constexpr std::size_t argument_count = sizeof...(types);
+    
+    template<std::size_t index>
+    using nth_argument_type = std::tuple_element_t<index,std::tuple<types...>>;
+};
+
+long function(int,float);
 
 int main()
 {
-    std::cout << "Placeholder" << std::endl;
+    using traits = function_traits<decltype(function)>;
+
+    std::cout << traits::argument_count << std::endl;
+
+    traits::return_type value0;
+    traits::nth_argument_type<0> value1;
+    traits::nth_argument_type<1> value2;
+
     std::cin.get();
     return 0;
 }
